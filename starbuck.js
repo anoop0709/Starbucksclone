78fe9d23dc11940883fc0521a465018d8056637f
Anoop <anoopsukumarannairk@gmail.com>
2022-08-19 11:43:14 +0530

    completed
    menu toggle in mobile and tab was responsive footer was finished

---
+++ starbuck.js
@@ -0,0 +1,64 @@
+function Aboutus(){
+    
+    var x = document.getElementById("aboutus");
+    if(x.style.display === "none"){
+        x.style.display = "flex";
+    }else{
+        x.style.display = "none";
+    }
+
+}
+function Carrers(){
+    
+    var x = document.getElementById("carrers");
+    if(x.style.display === "none"){
+        x.style.display = "flex";
+    }else{
+        x.style.display = "none";
+    }
+
+}
+function Socialimpact(){
+    
+    var x = document.getElementById("socialimpact");
+    if(x.style.display === "none"){
+        x.style.display = "flex";
+    }else{
+        x.style.display = "none";
+    }
+
+}
+function Business(){
+    
+    var x = document.getElementById("business");
+    if(x.style.display === "none"){
+        x.style.display = "flex";
+    }else{
+        x.style.display = "none";
+    }
+
+}
+function Order(){
+    
+    var x = document.getElementById("order");
+    if(x.style.display === "none"){
+        x.style.display = "flex";
+    }else{
+        x.style.display = "none";
+    }
+
+}
+
+function Menu(){
+    
+    var x = document.getElementById("menu");
+    if(x.style.display === "none"){
+        x.style.display = "flex";
+    }else{
+        x.style.display = "none";
+    }
+
+}
+        
+
+    
